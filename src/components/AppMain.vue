fe0a479389620c825ebbbef0a5e4d5b9db2ade75
AlfredoCastrogiovanni <castro.alfry@gmail.com>
2023-12-14 14:23:22 +0100

    create listcomics component

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,12 +1,16 @@
 <template>
     <main>
-        main
+        <ListComics/>
     </main>
 </template>
 
 <script>
+    import ListComics from './ListComics.vue'
+
     export default {
-        
+        components: {
+            ListComics
+        }
     }
 </script>
 
